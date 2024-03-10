8960e549e5b6a1000f242538cfcaa473ade9d0c4
MOHAMED ABDULLAHI MOHAMUD <44861583+ShurieJr@users.noreply.github.com>
2024-03-10 11:24:13 +0300

    updated

---
+++ src/CA2212/chapter3/TernaryOp.java
@@ -0,0 +1,18 @@
+package CA2212.chapter3;
+
+public class TernaryOp {
+    public static void main(String[] args) {
+        int age = 10;
+
+//        if (age > 15)
+//            System.out.println("Adult");
+//        else
+//            System.out.println("child");
+        //Ternary operator
+        System.out.println(
+                (age > 15) ? "Adult" : "Child"
+        );
+
+        int year = 2024;
+    }
+}
