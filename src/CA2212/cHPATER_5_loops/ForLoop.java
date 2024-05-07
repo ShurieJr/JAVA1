81afe551780f1da399a3d1a35a0125123fab4f83
MOHAMED ABDULLAHI MOHAMUD <44861583+ShurieJr@users.noreply.github.com>
2024-05-07 09:32:31 +0300

    updated

---
+++ src/CA2212/cHPATER_5_loops/ForLoop.java
@@ -0,0 +1,11 @@
+package CA2212.cHPATER_5_loops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        //for loop
+        int i;
+        for (i=0 ; i < 2 ; i++ ) {
+            System.out.println("welcome to java!");
+        }
+    }
+}
